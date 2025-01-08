c89c8111ad27343e1c34fb9f31017190676653bd
Benjamin Dehli <superelg@gmail.com>
2025-01-08 17:27:27 +0100

    Add helper functions

---
+++ src/functions/helpers.js
@@ -0,0 +1,71 @@
+export function objectHasValue(obj) {
+    for (let key in obj) {
+        return !!obj?.[key]?.toString().length > 0;
+    }
+    return false;
+}
+
+function addStyle(element, style) {
+    for (let key in style) {
+        element.style[key] = style[key];
+    }
+}
+
+export function renderFieldTitleElement(fieldTitle) {
+    const fieldTitleLabelElement = document.createElement("label");
+    fieldTitleLabelElement.classList.add("fds-label", "fds-label--md");
+    const fieldTitleSpanElement = document.createElement("span");
+    fieldTitleSpanElement.innerHTML = fieldTitle;
+    fieldTitleLabelElement.appendChild(fieldTitleSpanElement);
+    return fieldTitleLabelElement;
+}
+
+export function renderFieldValueElement(fieldValue) {
+    const fieldValueElement = document.createElement("span");
+    fieldValueElement.classList.add("fds-paragraph", "fds-paragraph--md");
+    fieldValueElement.innerHTML = fieldValue;
+    addStyle(fieldValueElement, {
+        whiteSpace: "pre-line"
+    });
+    return fieldValueElement;
+}
+
+export function renderFieldElement(fieldTitle, fieldValue, returnHtml = true, styleoverride) {
+    const fieldElement = document.createElement("div");
+    fieldElement.classList.add("field");
+    if (fieldTitle?.length) {
+        fieldElement.appendChild(renderFieldTitleElement(fieldTitle));
+    }
+    fieldElement.appendChild(renderFieldValueElement(fieldValue));
+    addStyle(fieldElement, {
+        ...styleoverride,
+        display: "flex",
+        flexDirection: "column"
+    });
+    return returnHtml ? fieldElement.outerHTML : fieldElement;
+}
+
+export function renderListElement(listItems, listType, returnHtml = true) {
+    const listElement = document.createElement(listType || "ul");
+    listElement.classList.add("fds-paragraph", "fds-paragraph--md", "fds-list", "fds-list--md");
+    listItems.forEach((listItem) => {
+        const listItemElement = document.createElement("li");
+        listItemElement.innerHTML = listItem;
+        listElement.appendChild(listItemElement);
+    });
+    return returnHtml ? listElement.outerHTML : listElement;
+}
+
+export function renderListFieldElement(fieldTitle, listItems, listType) {
+    const fieldElement = document.createElement("div");
+    fieldElement.classList.add("field");
+    if (fieldTitle?.length) {
+        fieldElement.appendChild(renderFieldTitleElement(fieldTitle));
+    }
+    fieldElement.appendChild(renderListElement(listItems, listType, false));
+    addStyle(fieldElement, {
+        display: "flex",
+        flexDirection: "column"
+    });
+    return fieldElement.outerHTML;
+}
