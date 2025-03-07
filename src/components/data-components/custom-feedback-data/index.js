0b9c910be1f4f0742c30b69be1fbed17c4428203
Benjamin Dehli <superelg@gmail.com>
2025-03-07 08:09:07 +0100

    Remove unused props and logic

--- src/components/data-components/custom-feedback-data/index.js
+++ src/components/data-components/custom-feedback-data/index.js
@@ -9,13 +9,13 @@ export default customElements.define(
     "custom-feedback-data",
     class extends HTMLElement {
         connectedCallback() {
-            const { formData, hideIfEmpty, emptyFieldText, styleOverride } = getCustomComponentProps(this);
+            const { formData, styleOverride } = getCustomComponentProps(this);
             const feedbackType = this.getAttribute("feedbackType");
             const componentContainerElement = getComponentContainerElement(this);
-            if (hideIfEmpty && !formData?.simpleBinding && !!componentContainerElement) {
+            const value = formData?.simpleBinding;
+            if (!hasValue(value) && !!componentContainerElement) {
                 componentContainerElement.style.display = "none";
             } else {
-                const value = hasValue(formData?.simpleBinding) ? formData?.simpleBinding : emptyFieldText;
                 this.innerHTML = createCustomElement("custom-feedback", {
                     formData: { simpleBinding: value },
                     feedbackType,
