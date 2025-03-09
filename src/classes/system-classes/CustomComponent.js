09dd483dc2b1f00b1fd92f54d865b8a4e1102c67
Benjamin Dehli <superelg@gmail.com>
2025-03-09 20:13:20 +0100

    Add classes for custom component and custom HTML attributes

---
+++ src/classes/system-classes/CustomComponent.js
@@ -0,0 +1,139 @@
+import { hasValue } from "../../functions/helpers.js";
+
+export default class CustomComponent {
+    constructor(element) {
+        const formData = this.getFormDataFromElement(element);
+        const text = this.getTextFromElement(element);
+        const texts = this.getTextsFromElement(element);
+        const inline = this.getInlineFromElement(element);
+        const hideTitle = this.getHideTitle(element);
+        const size = this.getSize(element);
+        const hideIfEmpty = this.getHideIfEmpty(element);
+        const emptyFieldText = this.getEmptyFieldText(element);
+        const styleOverride = this.getStyleOverride(element);
+        const isChildComponent = this.getIsChildComponent(element);
+        const tableColumns = this.getTableColumns(element);
+        const feedbackType = this.getFeedbackType(element);
+        const itemKey = this.getItemKey(element);
+        const hideOrgNr = this.getHideOrgNr(element);
+        if (formData) {
+            this.formData = formData;
+        }
+        if (text) {
+            this.text = text;
+        }
+        if (texts) {
+            this.texts = texts;
+        }
+        if (inline) {
+            this.inline = inline;
+        }
+        if (hideTitle) {
+            this.hideTitle = hideTitle;
+        }
+        if (size) {
+            this.size = size;
+        }
+        if (hideIfEmpty) {
+            this.hideIfEmpty = hideIfEmpty;
+        }
+        if (emptyFieldText) {
+            this.emptyFieldText = emptyFieldText;
+        }
+        if (styleOverride) {
+            this.styleOverride = styleOverride;
+        }
+        if (isChildComponent) {
+            this.isChildComponent = isChildComponent;
+        }
+        if (tableColumns) {
+            this.tableColumns = tableColumns;
+        }
+        if (feedbackType) {
+            this.feedbackType = feedbackType;
+        }
+        if (itemKey) {
+            this.itemKey = itemKey;
+        }
+        if (hideOrgNr) {
+            this.hideOrgNr = hideOrgNr;
+        }
+    }
+
+    getFormDataFromElement(element) {
+        const formData = JSON.parse(element?.getAttribute("formdata"));
+        return hasValue(formData) && formData;
+    }
+
+    getTextFromElement(element) {
+        const text = element?.getAttribute("text");
+        return hasValue(text) && text;
+    }
+
+    getTextsFromElement(element) {
+        const texts = JSON.parse(element?.getAttribute("texts"));
+        return hasValue(texts) && texts;
+    }
+
+    getInlineFromElement(element) {
+        return element?.getAttribute("inline") === "true";
+    }
+
+    getHideTitle(element) {
+        return element?.getAttribute("hideTitle") === "true";
+    }
+
+    getSize(element) {
+        const size = element?.getAttribute("size");
+        return hasValue(size) && size;
+    }
+
+    getHideIfEmpty(element) {
+        return element?.getAttribute("hideIfEmpty") === "true";
+    }
+
+    getEmptyFieldText(element) {
+        const emptyFieldText = element?.getAttribute("emptyFieldText");
+        return hasValue(emptyFieldText) && emptyFieldText;
+    }
+
+    getStyleOverride(element) {
+        const styleOverride = JSON.parse(element?.getAttribute("styleOverride") || "{}");
+        return hasValue(styleOverride) && styleOverride;
+    }
+
+    getIsChildComponent(element) {
+        return element?.getAttribute("isChildComponent") === "true";
+    }
+
+    getTableColumns(element) {
+        const tableColumns = JSON.parse(element?.getAttribute("tableColumns"));
+        return hasValue(tableColumns) && tableColumns;
+    }
+
+    getFeedbackType(element) {
+        const feedbackType = element?.getAttribute("feedbackType");
+        return hasValue(feedbackType) && feedbackType;
+    }
+
+    getItemKey(element) {
+        const itemKey = element?.getAttribute("itemKey");
+        return hasValue(itemKey) && itemKey;
+    }
+
+    getHideOrgNr(element) {
+        return element?.getAttribute("hideOrgNr") === "true";
+    }
+
+    setFormData(formData) {
+        this.formData = formData;
+    }
+
+    setTexts(texts) {
+        this.texts = texts;
+    }
+
+    setText(text) {
+        this.text = text;
+    }
+}
---
+++ src/classes/system-classes/CustomElementHtmlAttributes.js
@@ -0,0 +1,160 @@
+import { hasValue } from "../../functions/helpers.js";
+
+export default class CustomElementHtmlAttributes {
+    constructor(props) {
+        const isChildComponent = true;
+        const formData = this.getFormDataAttributeFromProps(props);
+        const text = this.getTextAttributeFromProps(props);
+        const size = this.getSizeAttributeFromProps(props);
+        const hideTitle = this.getHideTitleAttributeFromProps(props);
+        const hideIfEmpty = this.getHideIfEmptyAttributeFromProps(props);
+        const inline = this.getInlineAttributeFromProps(props);
+        const emptyFieldText = this.getEmptyFieldTextAttributeFromProps(props);
+        const styleOverride = this.getStyleOverrideAttributeFromProps(props);
+        const grid = this.getGridAttributeFromProps(props);
+        const texts = this.getTextsAttributeFromProps(props);
+        const tableColumns = this.getTableColumnsAttributeFromProps(props);
+        const textResources = this.getTextResourcesAttributeFromProps(props);
+        const itemKey = this.getItemKeyAttributeFromProps(props);
+        const id = this.getIdAttributeFromProps(props);
+        const feedbackType = this.getFeedbackTypeAttributeFromProps(props);
+        const hideOrgNr = this.getHideOrgNr(props);
+        if (isChildComponent) {
+            this.isChildComponent = "true";
+        }
+        if (formData) {
+            this.formData = formData;
+        }
+        if (text) {
+            this.text = text;
+        }
+        if (size) {
+            this.size = size;
+        }
+        if (hideTitle) {
+            this.hideTitle = hideTitle;
+        }
+        if (hideIfEmpty) {
+            this.hideIfEmpty = hideIfEmpty;
+        }
+        if (inline) {
+            this.inline = inline;
+        }
+        if (emptyFieldText) {
+            this.emptyFieldText = emptyFieldText;
+        }
+        if (styleOverride) {
+            this.styleOverride = styleOverride;
+        }
+        if (grid) {
+            this.grid = grid;
+        }
+        if (texts) {
+            this.texts = texts;
+        }
+        if (tableColumns) {
+            this.tableColumns = tableColumns;
+        }
+        if (textResources) {
+            this.textResources = textResources;
+        }
+        if (itemKey) {
+            this.itemKey = itemKey;
+        }
+        if (id) {
+            this.id = id;
+        }
+        if (feedbackType) {
+            this.feedbackType = feedbackType;
+        }
+        if (hideOrgNr) {
+            this.hideOrgNr = hideOrgNr;
+        }
+    }
+
+    getFormDataAttributeFromProps(props) {
+        if (hasValue(props?.formData)) {
+            if (typeof props?.formData === "string") {
+                const formData = props?.formData;
+                return JSON.stringify(formData);
+            } else if (typeof props?.formData === "number") {
+                const formData = props?.formData.toString();
+                return JSON.stringify(formData);
+            } else if (typeof props?.formData === "object") {
+                const formData = {};
+                Object.keys(props.formData).forEach((key) => {
+                    formData[key] = props.formData[key];
+                });
+                return JSON.stringify(formData);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    getTextAttributeFromProps(props) {
+        const textAttribute = props?.text?.toString() || props?.texts?.title?.toString() || "";
+        return !props?.hideTitle && textAttribute;
+    }
+
+    getSizeAttributeFromProps(props) {
+        return hasValue(props?.size) && props?.size?.toString();
+    }
+
+    getHideTitleAttributeFromProps(props) {
+        return props?.hideTitle?.toString() === "true" && "true";
+    }
+
+    getHideIfEmptyAttributeFromProps(props) {
+        return props?.hideIfEmpty?.toString() === "true" && "true";
+    }
+
+    getInlineAttributeFromProps(props) {
+        return props?.inline?.toString() === "true" && "true";
+    }
+
+    getEmptyFieldTextAttributeFromProps(props) {
+        return hasValue(props?.emptyFieldText) && props?.emptyFieldText.toString();
+    }
+
+    getStyleOverrideAttributeFromProps(props) {
+        return hasValue(props?.styleOverride) && JSON.stringify(props?.styleOverride);
+    }
+
+    getGridAttributeFromProps(props) {
+        return hasValue(props?.grid) && JSON.stringify(props?.grid);
+    }
+
+    getTextsAttributeFromProps(props) {
+        return hasValue(props?.texts) && JSON.stringify(props?.texts);
+    }
+
+    getTableColumnsAttributeFromProps(props) {
+        return hasValue(props?.tableColumns) && JSON.stringify(props?.tableColumns);
+    }
+
+    getTextResourcesAttributeFromProps(props) {
+        return hasValue(props?.textResources) && JSON.stringify(props?.textResources);
+    }
+
+    getItemKeyAttributeFromProps(props) {
+        return hasValue(props?.itemKey) && props?.itemKey;
+    }
+
+    getIdAttributeFromProps(props) {
+        return hasValue(props?.id) && props?.id;
+    }
+
+    getFeedbackTypeAttributeFromProps(props) {
+        const validFeedbackTypes = ["error", "warning", "success", "info", "default"];
+        if (hasValue(props?.feedbackType)) {
+            return validFeedbackTypes.includes(props?.feedbackType) ? props?.feedbackType : "default";
+        } else {
+            return null;
+        }
+    }
+
+    getHideOrgNr(props) {
+        return props?.hideOrgNr?.toString() === "true" && "true";
+    }
+}
