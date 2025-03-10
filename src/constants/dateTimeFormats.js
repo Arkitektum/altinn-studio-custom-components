54f4865225e7a37a00f514e3ac0c8010ea31c770
Benjamin Dehli <superelg@gmail.com>
2025-03-10 15:49:37 +0100

    Add default values for locale, date and time

---
+++ src/constants/dateTimeFormats.js
@@ -0,0 +1,38 @@
+export const dateTimeFormat = {
+    dateTime: {
+        default: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        }
+    },
+    date: {
+        default: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        }
+    },
+    time: {
+        default: {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        }
+    }
+};
+
+export const dateTimeLocale = {
+    dateTime: {
+        default: "no-NO"
+    },
+    date: {
+        default: "no-NO"
+    },
+    time: {
+        default: "no-NO"
+    }
+};
