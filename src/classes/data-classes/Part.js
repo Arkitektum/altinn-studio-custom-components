c4c46d312c104fc6697f9f74a22ddb9ab4c25aad
Benjamin Dehli <superelg@gmail.com>
2025-03-25 16:26:56 +0100

    Add properties to Part class

--- src/classes/data-classes/Part.js
+++ src/classes/data-classes/Part.js
@@ -1,16 +1,27 @@
+import Adresse from "./Adresse.js";
+import Telefonnumre from "./Telefonnumre.js";
+
 /**
  * Class representing a Part.
  * @class
  */
 export default class Part {
     /**
-     * Create a Part.
-     * @param {Object} props - The properties of the Part.
-     * @param {string} props.navn - The name of the Part.
-     * @param {string} props.organisasjonsnummer - The organization number of the Part.
+     * Creates an instance of the Part class.
+     *
+     * @param {Object} props - The properties to initialize the Part instance.
+     * @param {string} [props.navn] - The name of the part.
+     * @param {string} [props.organisasjonsnummer] - The organization number of the part.
+     * @param {Object} [props.adresse] - The address of the part, used to create an instance of the Adresse class.
+     * @param {string} [props.telefonnummer] - The landline phone number of the part.
+     * @param {string} [props.mobilnummer] - The mobile phone number of the part.
+     * @param {string} [props.epost] - The email address of the part.
      */
     constructor(props) {
         this.navn = props?.navn;
         this.organisasjonsnummer = props?.organisasjonsnummer;
+        this.adresse = props?.adresse && new Adresse(props.adresse);
+        this.telefonnumre = props?.telefonnummer || props?.mobilnummer ? new Telefonnumre(props) : null;
+        this.epost = props?.epost;
     }
 }
