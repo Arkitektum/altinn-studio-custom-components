a717d282e41e0ca833154b7eb1523c7d72e0cc4b
Benjamin Dehli <superelg@gmail.com>
2025-03-28 16:35:26 +0100

    Add and setup Jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: "jsdom",
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    testPathIgnorePatterns: ["/node_modules/"],
+    verbose: true
+};
