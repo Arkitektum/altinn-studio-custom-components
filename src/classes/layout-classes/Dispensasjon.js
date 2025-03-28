723c14d5052136627c3f72c23aaa36196dc0605c
Benjamin Dehli <superelg@gmail.com>
2025-03-28 10:38:14 +0100

    Add 'soeknadstype' to 'Dispensasjon' class

--- src/classes/layout-classes/Dispensasjon.js
+++ src/classes/layout-classes/Dispensasjon.js
@@ -2,6 +2,7 @@ import Begrunnelse from "../data-classes/Begrunnelse.js";
 import DispensasjonBeskrivelse from "../data-classes/DispensasjonBeskrivelse.js";
 import DispensasjonFra from "../data-classes/DispensasjonFra.js";
 import EiendomByggested from "../data-classes/EiendomByggested.js";
+import Kode from "../data-classes/Kode.js";
 import KommunensSaksnummer from "../data-classes/KommunensSaksnummer.js";
 import Metadata from "../data-classes/Metadata.js";
 import Part from "../data-classes/Part.js";
@@ -18,6 +19,7 @@ export default class Dispensasjon {
      * @param {Object} props - The properties to initialize the Dispensasjon instance.
      * @param {DispensasjonBeskrivelse} [props.dispensasjonBeskrivelse] - The description of the dispensasjon.
      * @param {string} [props.dispensasjonReferanse] - The reference for the dispensasjon.
+     * @param {Kode} [props.soeknadstype] - The type of application.
      * @param {KommunensSaksnummer} [props.kommunensSaksnummer] - The municipality's case number.
      * @param {Metadata} [props.metadata] - Metadata associated with the dispensasjon.
      * @param {EiendomByggested} [props.eiendomByggested] - The property or construction site information.
@@ -29,9 +31,9 @@ export default class Dispensasjon {
      * @param {string} [props.generelleVilkaar] - General conditions for the dispensasjon.
      */
     constructor(props) {
-        this.dispensasjonBeskrivelse =
-            props?.dispensasjonBeskrivelse && new DispensasjonBeskrivelse(props.dispensasjonBeskrivelse);
+        this.dispensasjonBeskrivelse = props?.dispensasjonBeskrivelse && new DispensasjonBeskrivelse(props.dispensasjonBeskrivelse);
         this.dispensasjonReferanse = props?.dispensasjonReferanse;
+        this.soeknadstype = props?.soeknadstype && new Kode(props.soeknadstype);
         this.kommunensSaksnummer = props?.kommunensSaksnummer && new KommunensSaksnummer(props.kommunensSaksnummer);
         this.metadata = props?.metadata && new Metadata(props.metadata);
         this.eiendomByggested = props?.eiendomByggested && new EiendomByggested(props.eiendomByggested);
