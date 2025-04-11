7c701ecc549abad9c94a6c14a489ac699b691468
Benjamin Dehli <superelg@gmail.com>
2025-04-11 16:01:50 +0200

    Clarifies JSDoc descriptions
    Updates the JSDoc descriptions in `Eiendomsidentifikasjon.js`
    for better clarity and conciseness. Removes redundant wording
    from parameter descriptions.

--- src/classes/data-classes/Eiendomsidentifikasjon.js
+++ src/classes/data-classes/Eiendomsidentifikasjon.js
@@ -7,10 +7,10 @@ export default class Eiendomsidentifikasjon {
      * Constructs an instance of Eiendomsidentifikasjon.
      *
      * @param {Object} props - The properties to initialize the instance with.
-     * @param {number} [props.gaardsnummer] - The gaardsnummer (farm number).
-     * @param {number} [props.bruksnummer] - The bruksnummer (usage number).
-     * @param {number} [props.seksjonsnummer] - The seksjonsnummer (section number).
-     * @param {number} [props.festenummer] - The festenummer (lease number).
+     * @param {number} [props.gaardsnummer] - The gaardsnummer.
+     * @param {number} [props.bruksnummer] - The bruksnummer.
+     * @param {number} [props.seksjonsnummer] - The seksjonsnummer.
+     * @param {number} [props.festenummer] - The festenummer.
      */
     constructor(props) {
         this.gaardsnummer = props?.gaardsnummer;
