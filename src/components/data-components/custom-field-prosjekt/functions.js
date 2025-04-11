fcb54ee1ca2f1a7b29a94d123ae7afb8ecfd3b3e
Benjamin Dehli <superelg@gmail.com>
2025-04-11 17:14:10 +0200

    Formats project with optional project number
    Updates the `formatProsjekt` function to handle cases where the project number is missing or not a string.
    
    It now correctly formats the project string, including the project number only if it has a value, and converts the project number to a string to avoid type issues.

--- src/components/data-components/custom-field-prosjekt/functions.js
+++ src/components/data-components/custom-field-prosjekt/functions.js
@@ -1,13 +1,17 @@
+// Global functions
+import { hasValue } from "../../../functions/helpers.js";
+
 /**
- * Formats a project object into a string containing the project name and project number.
+ * Formats a project object into a string containing the project name and project number (if available).
  *
  * @param {Object} prosjekt - The project object to format.
- * @param {string} prosjekt.prosjektnavn - The name of the project.
- * @param {string} [prosjekt.prosjektnr] - The number of the project.
- * @returns {string} The formatted project string.
+ * @param {string} [prosjekt.prosjektnavn] - The name of the project.
+ * @param {number|string} [prosjekt.prosjektnr] - The project number.
+ * @returns {string} A formatted string containing the project name and project number in parentheses,
+ *                   separated by a space. If either is missing, it will be excluded from the result.
  */
 export function formatProsjekt(prosjekt) {
-    const prosjektnrString = prosjekt?.prosjektnr?.length && `(${prosjekt.prosjektnr})`;
-    const prosjektParts = [prosjekt?.prosjektnavn, prosjektnrString];
+    const prosjektnrString = hasValue(prosjekt?.prosjektnr) && `(${prosjekt.prosjektnr?.toString()})`;
+    const prosjektParts = [prosjekt?.prosjektnavn?.toString(), prosjektnrString];
     return prosjektParts.filter((prosjektPart) => prosjektPart?.length).join(" ");
 }
