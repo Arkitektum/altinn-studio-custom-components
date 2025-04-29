594061d4ead7f19661f1c9ab3aaef160fa13e656
Benjamin Dehli <superelg@gmail.com>
2025-04-29 13:31:45 +0200

    Adds SoeknadGjelder data class
    Creates the `SoeknadGjelder` data class to handle and process
    the application's "SoeknadGjelder" data structure.
    
    Includes methods for extracting type information and converting
    type objects into arrays of `Kode` instances.
    
    Also includes corresponding unit tests to ensure the class
    functions as expected.

---
+++ src/classes/data-classes/SoeknadGjelder.js
@@ -0,0 +1,57 @@
+// Classes
+import Kode from "./Kode.js";
+
+// Global functions
+import { hasValue } from "../../functions/helpers.js";
+
+/**
+ * Class representing SoeknadGjelder.
+ * This class is used to handle the "SoeknadGjelder" data structure,
+ * extracting and processing type information from the provided properties.
+ * @class
+ */
+export default class SoeknadGjelder {
+    /**
+     * Creates an instance of SoeknadGjelder.
+     * @param {Object} props - The properties object.
+     * @param {Object} [props.type] - The type object containing type information.
+     * @param {Array} [props.type.kode] - An array of type codes.
+     */
+    constructor(props) {
+        const type = props ? this.getTypeFromProps(props) : null;
+        if (type) {
+            this.type = type;
+        }
+    }
+
+    /**
+     * Extracts the type information from the provided properties.
+     * @param {Object} props - The properties object.
+     * @param {Object} [props.type] - The type object containing type information.
+     * @param {Array} [props.type.kode] - An array of type codes.
+     * @returns {Object|null} An object containing the extracted type information, or null if not available.
+     */
+    getTypeFromProps(props) {
+        if (props && hasValue(props?.type)) {
+            return {
+                kode: this.getKodeFromType(props.type)
+            };
+        }
+        return null;
+    }
+
+    /**
+     * Converts the provided type object into an array of Kode instances.
+     * @param {Object} type - The type object containing type information.
+     * @param {Array} [type.kode] - An array of type codes.
+     * @returns {Array|null} An array of Kode instances, or null if no valid codes are found.
+     */
+    getKodeFromType(type) {
+        if (Array.isArray(type?.kode) && type?.kode?.length) {
+            return type?.kode?.map((item) => {
+                return new Kode(item);
+            });
+        }
+        return null;
+    }
+}
