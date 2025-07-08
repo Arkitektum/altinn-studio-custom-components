2925b0f663177bea9da59e0dff3e7eb5bc315ce5
Benjamin Dehli <superelg@gmail.com>
2025-07-08 14:55:25 +0200

    Refactors address component for data handling
    Updates the address component to retrieve and format data using helper functions.
    
    Improves data handling by extracting value retrieval and formatting logic. It also leverages text resources for title and empty field states.

--- src/classes/system-classes/component-classes/CustomFieldAdresse.js
+++ src/classes/system-classes/component-classes/CustomFieldAdresse.js
@@ -3,33 +3,21 @@ import Adresse from "../../data-classes/Adresse.js";
 import CustomComponent from "../CustomComponent.js";
 
 // Global functions
-import { hasValue } from "../../../functions/helpers.js";
+import { getComponentDataValue, getTextResourceFromResourceBinding, hasValue } from "../../../functions/helpers.js";
 
-/**
- * CustomFieldAdresse is a custom component class for handling and formatting address data.
- *
- * Extends the CustomComponent class and provides methods to:
- * - Format address lines into a single string.
- * - Format zip code and city into a single string.
- * - Format a complete address object into a displayable string.
- * - Extract and format form data from component properties.
- * - Determine if the address form data contains any content.
- *
- * @class
- * @extends CustomComponent
- */
 export default class CustomFieldAdresse extends CustomComponent {
-    constructor(element) {
-        super(element);
-        const props = element instanceof HTMLElement ? super.getPropsFromElementAttributes(element) : element;
+    constructor(props) {
+        super(props);
+        const data = this.getValueFromFormData(props);
 
-        const formData = this.getFormDataFromProps(props);
-        const isEmpty = !this.hasContent(formData);
-
-        this.isEmpty = isEmpty;
-        this.formData = formData;
+        const isEmpty = !this.hasContent(data);
 
         this.isEmpty = isEmpty;
+
+        this.resourceValues = {
+            title: getTextResourceFromResourceBinding(props?.resourceBindings?.title),
+            data: isEmpty ? getTextResourceFromResourceBinding(props?.resourceBindings?.emptyFieldText) : data
+        };
     }
 
     /**
@@ -72,31 +60,34 @@ export default class CustomFieldAdresse extends CustomComponent {
         return adresseLinje?.length ? `${adresseLinje}\n${zipCity}` : zipCity;
     }
 
-    getFormDataFromProps(element) {
-        const data = element?.formData?.data;
+    /**
+     * Retrieves and formats the address value from the provided form data props.
+     *
+     * @param {Object} props - The properties containing form data for the component.
+     * @returns {string} The formatted address string.
+     */
+    getValueFromFormData(props) {
+        const data = getComponentDataValue(props);
         const address = new Adresse(data);
         const adresseString = this.formatAdresse(address);
-        return {
-            simpleBinding: adresseString
-        };
+        return adresseString;
     }
 
     /**
-     * Determines if the provided form data contains any address information.
+     * Determines if the provided data contains any address information.
      *
      * Checks if any of the address lines (adresselinje1, adresselinje2, adresselinje3)
      * or either the postal code (postnr) or city (poststed) fields have content.
      *
-     * @param {Object} formData - The form data object containing address information.
-     * @param {Object} formData.data - The address data object.
-     * @param {string} [formData.data.adresselinje1] - First address line.
-     * @param {string} [formData.data.adresselinje2] - Second address line.
-     * @param {string} [formData.data.adresselinje3] - Third address line.
-     * @param {string} [formData.data.postnr] - Postal code.
-     * @param {string} [formData.data.poststed] - City.
+     * @param {Object} data - The data object containing address information.
+     * @param {string} [data.adresselinje1] - First address line.
+     * @param {string} [data.adresselinje2] - Second address line.
+     * @param {string} [data.adresselinje3] - Third address line.
+     * @param {string} [data.postnr] - Postal code.
+     * @param {string} [data.poststed] - City.
      * @returns {boolean} True if any address line, postal code, or city has content; otherwise, false.
      */
-    hasContent(formData) {
-        return hasValue(formData?.simpleBinding);
+    hasContent(data) {
+        return hasValue(data);
     }
 }
