590a986c3b5de4c8f6f1d16a1f05a5ed5b655edb
Mantas Lapinskas <mantas@arkitektum.no>
2025-08-11 15:41:29 +0200

    Adds custom paragraph data component
    Adds a new custom paragraph data component that dynamically renders a paragraph based on data model bindings and text resources.
    The component handles cases where the bound data is empty, hiding itself if configured to do so.

---
+++ src/components/data-components/custom-paragraph-data/index.js
@@ -0,0 +1,30 @@
+// Classes
+import CustomComponent from "../../../classes/system-classes/CustomComponent.js";
+import CustomElementHtmlAttributes from "../../../classes/system-classes/CustomElementHtmlAttributes.js";
+
+// Global functions
+import { createCustomElement, getComponentContainerElement } from "../../../functions/helpers.js";
+
+// Local functions
+import { getFormDataValue } from "./functions.js";
+
+
+export default customElements.define(
+    "custom-paragraph-data",
+    class extends HTMLElement {
+        connectedCallback() {
+            const component = new CustomComponent(this);
+            const componentContainerElement = getComponentContainerElement(this);
+            if (component?.hideIfEmpty && !component?.formData?.simpleBinding && !!componentContainerElement) {
+                componentContainerElement.style.display = "none";
+            } else {
+                component.setFormData({
+                    simpleBinding: getFormDataValue(component)
+                });
+                const htmlAttributes = new CustomElementHtmlAttributes(component);
+                this.innerHTML = createCustomElement("custom-paragraph", htmlAttributes).outerHTML;
+            }
+        }
+    }
+);
+
