386ad55977fd97899a5acfdccb56f02a467273c4
Benjamin Dehli <superelg@gmail.com>
2025-08-20 15:50:43 +0200

    Adds handling for empty and validated group lists
    Improves the rendering of custom group lists by:
    
    - Displaying a message when the list is empty.
    - Rendering a header element for the list.
    - Rendering validation messages in a feedback list.
    
    These changes ensure a more user-friendly and informative experience.

--- src/components/data-components/custom-grouplist-utfall-svar-type/custom-group-utfall-svar-type/custom-grouplist-utfall-svar/renderers.js
+++ src/components/data-components/custom-grouplist-utfall-svar-type/custom-group-utfall-svar-type/custom-grouplist-utfall-svar/renderers.js
@@ -5,6 +5,24 @@ import CustomElementHtmlAttributes from "../../../../../classes/system-classes/C
 import { createCustomElement } from "../../../../../functions/helpers.js";
 
 /**
+ * Renders a custom header element for a given component.
+ *
+ * @param {Object} component - The component object containing resource values.
+ * @param {string} [size="h2"] - The size of the header element (e.g., "h1", "h2", etc.).
+ * @returns {HTMLElement} The created custom header element.
+ */
+export function renderHeaderElement(component, size = "h2") {
+    const htmlAttributes = new CustomElementHtmlAttributes({
+        isChildComponent: true,
+        size,
+        resourceValues: {
+            title: component?.resourceValues?.title
+        }
+    });
+    return createCustomElement("custom-header-text", htmlAttributes);
+}
+
+/**
  * Renders a custom group element for "utfallSvar" data.
  *
  * @param {Object} utfallSvar - The data to be rendered within the custom group component.
@@ -19,4 +37,22 @@ export function renderUtfallSvarGroup(utfallSvar) {
         }
     });
     return createCustomElement("custom-group-utfall-svar", htmlAttributes);
+}
+
+/**
+ * Renders a custom paragraph element displaying the empty field text for a given component.
+ *
+ * @param {Object} component - The component object containing resource values.
+ * @param {Object} [component.resourceValues] - Resource values for the component.
+ * @param {string} [component.resourceValues.data] - The text to display as the empty field.
+ * @returns {HTMLElement} The custom paragraph element with the specified attributes.
+ */
+export function renderEmptyFieldText(component) {
+    const htmlAttributes = new CustomElementHtmlAttributes({
+        isChildComponent: true,
+        resourceValues: {
+            title: component?.resourceValues?.data
+        }
+    });
+    return createCustomElement("custom-paragraph", htmlAttributes);
 }
