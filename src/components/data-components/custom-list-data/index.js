b44671e87fdb00da7d48dfb2d6eff55c8d67cb7b
Benjamin Dehli <superelg@gmail.com>
2025-09-03 13:08:46 +0200

    Removes unused list item extraction logic
    Removes the specific logic for extracting list items based on a key
    directly within the component.
    
    This simplifies the component's responsibilities and assumes that the
    data is pre-processed or formatted appropriately before being passed to
    the component.

--- src/components/data-components/custom-list-data/index.js
+++ src/components/data-components/custom-list-data/index.js
@@ -5,18 +5,12 @@ import { instantiateComponent } from "../../../functions/componentHelpers.js";
 // Global functions
 import { createCustomElement, getComponentContainerElement } from "../../../functions/helpers.js";
 
-// Local functions
-import { getListItemsFromKey } from "./functions.js";
-
 export default customElements.define(
     "custom-list-data",
     class extends HTMLElement {
         connectedCallback() {
             const component = new instantiateComponent(this);
             const componentContainerElement = getComponentContainerElement(this);
-            const listItems = component?.itemKey?.length
-                ? getListItemsFromKey(component?.formData?.data, component?.itemKey)
-                : component?.formData?.data;
             if (component?.hideIfEmpty && component.isEmpty && !!componentContainerElement) {
                 componentContainerElement.style.display = "none";
             } else {
