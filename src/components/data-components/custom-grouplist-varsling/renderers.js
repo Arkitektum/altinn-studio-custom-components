abb5440655bddbb85f9c651984f13d1b3e86755c
Benjamin Dehli <superelg@gmail.com>
2025-10-01 09:54:07 +0200

    Updates custom group list renderer
    Refactors the custom group list renderer to improve configuration options.
    
    Allows more flexible control over component behavior by enabling configuration of `hideIfEmpty` and other attributes directly through the component properties.

--- src/components/data-components/custom-grouplist-varsling/renderers.js
+++ src/components/data-components/custom-grouplist-varsling/renderers.js
@@ -4,16 +4,22 @@ import CustomElementHtmlAttributes from "../../../classes/system-classes/CustomE
 // Global functions
 import { createCustomElement } from "../../../functions/helpers.js";
 
+/**
+ * Renders a custom group list component for "sjekklistekrav" (checklist requirements).
+ *
+ * @param {Object} component - The component configuration object.
+ * @param {boolean} [component.hideIfEmpty] - Whether to hide the component if it is empty.
+ * @param {boolean} [component.enableLinks] - Whether to enable links within the component.
+ * @param {Object} [component.resourceBindings] - Resource bindings for the component.
+ * @param {Object} [component.resourceValues] - Resource values for the component.
+ * @returns {HTMLElement} The rendered custom group list element.
+ */
 export function renderSjekklistekravGroupList(component) {
     const htmlAttributes = new CustomElementHtmlAttributes({
         isChildComponent: true,
-        hideIfEmpty: true,
+        hideIfEmpty: component?.hideIfEmpty,
         enableLinks: component?.enableLinks,
-        resourceBindings: {
-            trueText: component?.resourceBindings?.trueText,
-            falseText: component?.resourceBindings?.falseText,
-            defaultText: component?.resourceBindings?.defaultText
-        },
+        resourceBindings: component?.resourceBindings,
         resourceValues: component?.resourceValues
     });
     return createCustomElement("custom-grouplist-sjekklistekrav", htmlAttributes);
