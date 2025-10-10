d0e4b8f0e97c8c8c176d04b9c52f430e17cc5773
Mantas Lapinskas <mantas@arkitektum.no>
2025-10-10 14:48:52 +0200

    Adds municipality name to address
    Extends the address functionality to include and display the municipality name (kommunenavn).
    
    This enhancement improves address representation and provides more complete location information in the CustomFieldAdresse component and Eiendom class. It includes handling for cases where the municipality name might be missing, ensuring a consistent user experience.

--- src/classes/data-classes/Eiendom.js
+++ src/classes/data-classes/Eiendom.js
@@ -11,15 +11,17 @@ export default class Eiendom {
      * Constructs an instance of the Eiendom class.
      *
      * @param {Object} props - The properties to initialize the Eiendom instance.
-     * @param {Object} [props.adresse] - The address information, used to create an instance of the Adresse class.
-     * @param {Object} [props.eiendomsidentifikasjon] - The property identification, used to create an instance of the Eiendomsidentifikasjon class.
-     * @param {string} [props.bolignummer] - The housing unit number.
+     * @param {Object} [props.adresse] - The address information, used to create an Adresse instance.
+     * @param {Object} [props.eiendomsidentifikasjon] - The property identification, used to create an Eiendomsidentifikasjon instance.
+     * @param {string} [props.bolignummer] - The housing number.
      * @param {string} [props.bygningsnummer] - The building number.
+     * @param {string|null} [props.kommunenavn] - The name of the municipality.
      */
     constructor(props) {
-        this.adresse = props?.adresse && new Adresse(props.adresse);
+        this.adresse = props?.adresse ? new Adresse(props.adresse) : new Adresse(props);
         this.eiendomsidentifikasjon = props?.eiendomsidentifikasjon && new Eiendomsidentifikasjon(props.eiendomsidentifikasjon);
         this.bolignummer = props?.bolignummer;
         this.bygningsnummer = props?.bygningsnummer;
+        this.kommunenavn = props?.kommunenavn || null;
     }
 }
