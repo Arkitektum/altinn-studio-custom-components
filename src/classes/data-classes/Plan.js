f77cb3a0f3ba983bc1eb80f80a105afb62341b5d
Benjamin Dehli <superelg@gmail.com>
2025-10-15 13:47:04 +0200

    Adds JSDoc comments to Plan class
    Adds JSDoc comments to the Plan class.
    This improves code readability and maintainability
    by providing clear documentation for the class's
    properties and constructor.
    
    Related to #87

--- src/classes/data-classes/Plan.js
+++ src/classes/data-classes/Plan.js
@@ -1,6 +1,13 @@
 // Global functions
 import Kode from "./Kode.js";
 
+/**
+ * Represents a Plan with a name and type.
+ * @class
+ * @param {Object} props - Properties for initializing the Plan.
+ * @param {string} [props.navn] - The name of the plan.
+ * @param {Object} [props.plantype] - The type of the plan, passed to the Kode constructor.
+ */
 export default class Plan {
     constructor(props) {
         this.navn = props?.navn;
