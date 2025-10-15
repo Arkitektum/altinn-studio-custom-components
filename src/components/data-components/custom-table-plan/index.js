fbdef9d06f1e93d0085948027ab0bcd0a5071821
Benjamin Dehli <superelg@gmail.com>
2025-10-15 13:31:45 +0200

    Adds custom table plan component element
    Adds a new component for rendering custom table plans.
    
    This component allows for dynamic table generation based on
    configured resource bindings, facilitating the display of
    structured plan data. It supports customizable columns and
    attributes, enhancing the flexibility of data presentation.

---
+++ src/components/data-components/custom-table-plan/index.js
@@ -0,0 +1,30 @@
+// Global functions
+import { instantiateComponent } from "../../../functions/componentHelpers.js";
+import { renderFeedbackListElement } from "../../../functions/feedbackHelpers.js";
+import { getComponentContainerElement } from "../../../functions/helpers.js";
+
+// Local functions
+import { renderPlanTable } from "./renderers.js";
+
+// Stylesheet
+import "./styles.css" with { type: "css" };
+
+export default customElements.define(
+    "custom-table-plan",
+    class extends HTMLElement {
+        async connectedCallback() {
+            const component = instantiateComponent(this);
+            const componentContainerElement = getComponentContainerElement(this);
+            if (component?.hideIfEmpty && component.isEmpty && !!componentContainerElement) {
+                componentContainerElement.style.display = "none";
+            } else {
+                const feedbackListElement = component.hasValidationMessages && renderFeedbackListElement(component?.validationMessages);
+                const planTableElement = renderPlanTable(component);
+                this.appendChild(planTableElement);
+                if (feedbackListElement) {
+                    this.appendChild(feedbackListElement);
+                }
+            }
+        }
+    }
+);
