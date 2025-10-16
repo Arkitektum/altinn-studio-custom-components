decb1173d17189ae64b9a5453e230d2ae817f01f
Benjamin Dehli <superelg@gmail.com>
2025-10-16 09:44:17 +0200

    Adds kodeverdi property to Kode class
    Adds the `kodeverdi` property to the `Kode` data class.
    
    This property represents the value of a code and is used in the
    `kode` component for displaying and handling custom field codes.
    
    Related to feature/89-add-custom-field-kode-component

--- src/classes/data-classes/Kode.js
+++ src/classes/data-classes/Kode.js
@@ -6,9 +6,11 @@ export default class Kode {
     /**
      * Create a Kode.
      * @param {Object} props - The properties object.
+     * @param {string} props.kodeverdi - The value code.
      * @param {string} props.kodebeskrivelse - The description code.
      */
     constructor(props) {
+        this.kodeverdi = props?.kodeverdi;
         this.kodebeskrivelse = props?.kodebeskrivelse;
     }
 }
