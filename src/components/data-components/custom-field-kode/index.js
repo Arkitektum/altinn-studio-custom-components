bf362af1771b4c2c224846e768ca0e721ab183fa
Benjamin Dehli <superelg@gmail.com>
2025-10-16 09:53:32 +0200

    Adds custom field kode component
    Adds a new custom element, "custom-field-kode", which handles the display logic for custom fields, including hiding empty fields and rendering the field with appropriate HTML attributes.
    
    Addresses issue where custom fields were not being rendered correctly.
    
    Fixes #89

---
+++ src/components/data-components/custom-field-kode/index.js
@@ -0,0 +1,22 @@
+// Classes
+import CustomElementHtmlAttributes from "../../../classes/system-classes/CustomElementHtmlAttributes.js";
+
+// Global functions
+import { createCustomElement, getComponentContainerElement } from "../../../functions/helpers.js";
+import { instantiateComponent } from "../../../functions/componentHelpers.js";
+
+export default customElements.define(
+    "custom-field-kode",
+    class extends HTMLElement {
+        connectedCallback() {
+            const component = instantiateComponent(this);
+            const componentContainerElement = getComponentContainerElement(this);
+            if (component?.hideIfEmpty && component.isEmpty && !!componentContainerElement) {
+                componentContainerElement.style.display = "none";
+            } else {
+                const htmlAttributes = new CustomElementHtmlAttributes(component);
+                this.innerHTML = createCustomElement("custom-field", htmlAttributes).outerHTML;
+            }
+        }
+    }
+);
