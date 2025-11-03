10e4b51f3f50ff876eb3354154b1c2501ab58c8d
Benjamin Dehli <superelg@gmail.com>
2025-11-03 14:31:25 +0100

    Adds custom table components for docs
    Adds custom table components to display data, specifically for "eiendom" and "tiltakshaver.kontaktperson".
    This provides structured presentation of information on the documentation page.

---
+++ public/docs/components/custom-table/custom-table-part.js
@@ -0,0 +1,11 @@
+export default {
+    id: "tiltakshaver.kontaktperson",
+    type: "Custom",
+    tagName: "custom-table-part",
+    size: "h2",
+    partType: "tiltakshaver.kontaktperson",
+    hideIfEmpty: true,
+    dataModelBindings: {
+        data: "tiltakshaver"
+    }
+};
