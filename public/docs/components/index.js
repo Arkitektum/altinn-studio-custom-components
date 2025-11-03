68ae8cd48edb8624bbe354ccd1a9c55e0e5912c7
Benjamin Dehli <superelg@gmail.com>
2025-11-03 14:32:22 +0100

    Creates component index for documentation
    Sets up an index file to manage and export custom field and table components, streamlining their usage within the documentation.

---
+++ public/docs/components/index.js
@@ -0,0 +1,15 @@
+// Field
+import customFieldAdresse from "./custom-field/custom-field-adresse.js";
+import customFieldData from "./custom-field/custom-field-data.js";
+
+// Table
+import customTableEiendom from "./custom-table/custom-table-eiendom.js";
+import customTablePart from "./custom-table/custom-table-part.js";
+
+export const field = { customFieldAdresse, customFieldData };
+export const table = { customTableEiendom, customTablePart };
+
+export default {
+    field,
+    table
+};
