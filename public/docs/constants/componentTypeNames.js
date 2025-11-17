68ab0d273d26be01ae34d96c192f9ad957c178ce
Benjamin Dehli <superelg@gmail.com>
2025-11-17 14:40:35 +0100

    Adds helper function for component type names
    Adds a helper function to retrieve component type names
    based on a provided key. This improves code readability
    and maintainability when working with component types.

---
+++ public/docs/constants/componentTypeNames.js
@@ -0,0 +1,10 @@
+export default {
+    typography: "Typografi",
+    field: "Felt",
+    list: "Liste",
+    descriptionList: "Beskrivelsesliste",
+    table: "Tabell",
+    summation: "Summering",
+    grouplist: "Gruppliste",
+    layout: "Underskjema"
+};
--- public/docs/scripts/helpers.js
+++ public/docs/scripts/helpers.js
@@ -1,6 +1,13 @@
+// Constants
 import componentNames from "../constants/componentNames";
+import componentTypeNames from "../constants/componentTypeNames";
 
 export function getComponentNameFromTagName(tagName) {
     const componentNameEntry = Object.entries(componentNames).find(([key, value]) => key === tagName);
     return componentNameEntry ? componentNameEntry[1] : tagName;
+}
+
+export function getComponentTypeNameFromKey(key) {
+    const componentTypeNameEntry = Object.entries(componentTypeNames).find(([typeKey, typeName]) => typeKey === key);
+    return componentTypeNameEntry ? componentTypeNameEntry[1] : key;
 }
