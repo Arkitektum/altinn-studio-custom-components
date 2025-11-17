cd1accb4f43b51a212feb633370680a44468c0f5
Benjamin Dehli <superelg@gmail.com>
2025-11-17 12:42:00 +0100

    Adds icons to component example titles
    Enhances the appearance and visual clarity of component example titles
    by adding relevant icons. This change improves the user experience
    by providing visual cues that quickly identify the type of example
    being displayed (markup, data, resources).

--- public/docs/scripts/renderers.js
+++ public/docs/scripts/renderers.js
@@ -1,8 +1,12 @@
 // Global functions
 import { appendChildren, hasValue } from "../../../src/functions/helpers.js";
 import { getComponentNameFromTagName } from "./helpers.js";
+
 // Assets
 import iconBrick from "../assets/svg/brick.svg";
+import iconCodeBlocks from "../assets/svg/code-blocks.svg";
+import iconDataObject from "../assets/svg/data-object.svg";
+import iconDictionary from "../assets/svg/dictionary.svg";
 
 /**
  * Renders a markup example for a component as a collapsible <details> element.
@@ -20,7 +24,12 @@ function renderComponentExampleMarkup(componentExample) {
 
     const titleElement = document.createElement("summary");
     titleElement.classList.add("component-example-markup-title");
+    const iconElement = document.createElement("img");
+    iconElement.src = iconCodeBlocks;
+    iconElement.alt = "Markup Icon";
+    iconElement.classList.add("summary-icon");
     titleElement.textContent = "Markup";
+    titleElement.prepend(iconElement);
     containerElement.appendChild(titleElement);
 
     const codeElement = document.createElement("pre");
@@ -45,7 +54,12 @@ function renderComponentExampleData(componentExampleData) {
 
     const titleElement = document.createElement("summary");
     titleElement.classList.add("component-example-data-title");
+    const iconElement = document.createElement("img");
+    iconElement.src = iconDataObject;
+    iconElement.alt = "Data Icon";
+    iconElement.classList.add("summary-icon");
     titleElement.textContent = "Data";
+    titleElement.prepend(iconElement);
     containerElement.appendChild(titleElement);
 
     const codeElement = document.createElement("pre");
@@ -70,7 +84,12 @@ function renderComponentExampleResources(componentExampleResources) {
 
     const titleElement = document.createElement("summary");
     titleElement.classList.add("component-example-resources-title");
+    const iconElement = document.createElement("img");
+    iconElement.src = iconDictionary;
+    iconElement.alt = "Resources Icon";
+    iconElement.classList.add("summary-icon");
     titleElement.textContent = "Resources";
+    titleElement.prepend(iconElement);
     containerElement.appendChild(titleElement);
 
     const codeElement = document.createElement("pre");
