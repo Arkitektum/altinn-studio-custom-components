7a6add302f28f221988ad9c4ede175a1adf6b14a
Benjamin Dehli <superelg@gmail.com>
2025-11-26 17:52:49 +0100

    Adds custom grouplist component definition
    Defines the markup and default resource bindings for a custom grouplist component related to neighbor property information.
    
    This new component configuration enables the display of detailed information about neighboring properties, including their address, property identification, and owner details, leveraging resource bindings for localization.

---
+++ public/docs/components/grouplist/custom-grouplist-nabo-gjenboer-eiendom.js
@@ -0,0 +1,83 @@
+const markup = {
+    id: "custom-grouplist-nabo-gjenboer-eiendom",
+    type: "Custom",
+    tagName: "custom-grouplist-nabo-gjenboer-eiendom",
+    hideTitle: false,
+    hideIfEmpty: false,
+    size: "h2",
+    dataModelBindings: {
+        data: "customGrouplist.naboGjenboerEiendom"
+    }
+};
+
+const defaultResourceBindings = {
+    eiendomMatrikkelinformasjon: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.title"
+    },
+    eiendomMatrikkelinformasjonAdresse: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.adresse.title",
+        emptyFieldText: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.adresse.emptyFieldText"
+    },
+    eiendomMatrikkelinformasjonEiendomsidentifikasjonGaardsnummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.eiendomsidentifikasjon.gaardsnummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eiendomMatrikkelinformasjonEiendomsidentifikasjonBruksnummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.eiendomsidentifikasjon.bruksnummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eiendomMatrikkelinformasjonEiendomsidentifikasjonSeksjonsnummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.eiendomsidentifikasjon.seksjonsnummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eiendomMatrikkelinformasjonEiendomsidentifikasjonFestenummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.eiendomsidentifikasjon.festenummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eiendomMatrikkelinformasjonBolignummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.bolignummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eiendomMatrikkelinformasjonBygningsnummer: {
+        title: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.bygningsnummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eier: {
+        title: "resource.eier.header"
+    },
+    eierNavn: {
+        title: "resource.eier.navn.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eierTelefonnummer: {
+        title: "resource.eier.telefonnummer.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eierEpost: {
+        title: "resource.eier.epost.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    eierAdresse: {
+        title: "resource.eier.adresse.title"
+    },
+    responsNabovarselSendtVia: {
+        title: "resource.respons.nabovarselSendtVia.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    responsMerknadMottattDato: {
+        title: "resource.respons.merknadMottattDato.title",
+        emptyFieldText: "resource.emptyFieldText.default"
+    },
+    responsErMerknadEllerSamtykkeMottatt: {
+        title: "resource.respons.erMerknadEllerSamtykkeMottatt.title",
+        falseText: "resource.respons.erMerknadEllerSamtykkeMottatt.falseText"
+    },
+    responsErMerknadMottatt: {
+        trueText: "resource.respons.erMerknadMottatt.trueText"
+    },
+    responsErSamtykkeMottatt: {
+        trueText: "resource.respons.erSamtykkeMottatt.trueText"
+    }
+};
+
+export default { markup, defaultResourceBindings };
