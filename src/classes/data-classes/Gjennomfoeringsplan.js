b549a5313535d446f3787c4cc9a548368733e573
Benjamin Dehli <superelg@gmail.com>
2025-11-26 14:44:11 +0100

    Adds JSDoc comments for Gjennomfoeringsplan
    Adds comprehensive JSDoc comments to the Gjennomfoeringsplan class,
    clarifying its purpose, constructor parameters, and properties. This
    enhances code readability and maintainability.

--- src/classes/data-classes/Gjennomfoeringsplan.js
+++ src/classes/data-classes/Gjennomfoeringsplan.js
@@ -4,7 +4,22 @@ import KommunensSaksnummer from "./KommunensSaksnummer.js";
 import Metadata from "./Metadata.js";
 import Part from "./Part.js";
 
+/**
+ * Class representing a Gjennomfoeringsplan.
+ * @class
+ */
 export default class Gjennomfoeringsplan {
+    /**
+     * Constructs a new Gjennomfoeringsplan instance.
+     *
+     * @param {Object} props - The properties to initialize the Gjennomfoeringsplan.
+     * @param {Object} [props.ansvarligSoeker] - Data for the ansvarligSoeker property, used to instantiate a Part.
+     * @param {Object} [props.eiendomByggested] - Data for the eiendomByggested property, used to instantiate an EiendomByggested.
+     * @param {*} [props.gjennomfoeringsplan] - The gjennomfoeringsplan data.
+     * @param {Object} [props.kommunensSaksnummer] - Data for the kommunensSaksnummer property, used to instantiate a KommunensSaksnummer.
+     * @param {Object} [props.metadata] - Data for the metadata property, used to instantiate a Metadata.
+     * @param {*} [props.versjon] - The version of the Gjennomfoeringsplan.
+     */
     constructor(props) {
         this.ansvarligSoeker = props?.ansvarligSoeker && new Part(props.ansvarligSoeker);
         this.eiendomByggested = props?.eiendomByggested && new EiendomByggested(props.eiendomByggested);
