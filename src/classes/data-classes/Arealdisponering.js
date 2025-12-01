a9316f90608a26d3fc4d00397a8cb06a50437a99
Benjamin Dehli <superelg@gmail.com>
2025-12-01 15:05:12 +0100

    Adds missing property to Arealdisponering
    The Arealdisponering data class was missing the
    'tomtearealSomLeggesTil' property. This commit adds the
    missing property to ensure data consistency.

--- src/classes/data-classes/Arealdisponering.js
+++ src/classes/data-classes/Arealdisponering.js
@@ -6,27 +6,29 @@ export default class Arealdisponering {
     /**
      * Creates an instance of Arealdisponering.
      * @param {Object} props - The properties of the area disposition.
-     * @param {number} [props.tomtearealByggeomraade] - The plot area of the building area.
-     * @param {number} [props.tomtearealSomTrekkesFra] - The plot area to be deducted.
-     * @param {number} [props.tomtearealBeregnet] - The calculated plot area.
-     * @param {number} [props.beregnetMaksByggeareal] - The calculated maximum building area.
      * @param {number} [props.arealBebyggelseEksisterende] - The existing building area.
-     * @param {number} [props.arealBebyggelseSomSkalRives] - The building area to be demolished.
      * @param {number} [props.arealBebyggelseNytt] - The new building area.
-     * @param {number} [props.parkeringsarealTerreng] - The parking area on the terrain.
+     * @param {number} [props.arealBebyggelseSomSkalRives] - The building area to be demolished.
      * @param {number} [props.arealSumByggesak] - The total area for the building case.
      * @param {number} [props.beregnetGradAvUtnytting] - The calculated degree of utilization.
+     * @param {number} [props.beregnetMaksByggeareal] - The calculated maximum building area.
+     * @param {number} [props.parkeringsarealTerreng] - The parking area on the terrain.
+     * @param {number} [props.tomtearealBeregnet] - The calculated plot area.
+     * @param {number} [props.tomtearealByggeomraade] - The plot area of the building area.
+     * @param {number} [props.tomtearealSomLeggesTil] - The plot area to be added.
+     * @param {number} [props.tomtearealSomTrekkesFra] - The plot area to be deducted.
      */
     constructor(props) {
-        this.tomtearealByggeomraade = props?.tomtearealByggeomraade;
-        this.tomtearealSomTrekkesFra = props?.tomtearealSomTrekkesFra;
-        this.tomtearealBeregnet = props?.tomtearealBeregnet;
-        this.beregnetMaksByggeareal = props?.beregnetMaksByggeareal;
         this.arealBebyggelseEksisterende = props?.arealBebyggelseEksisterende;
-        this.arealBebyggelseSomSkalRives = props?.arealBebyggelseSomSkalRives;
         this.arealBebyggelseNytt = props?.arealBebyggelseNytt;
-        this.parkeringsarealTerreng = props?.parkeringsarealTerreng;
+        this.arealBebyggelseSomSkalRives = props?.arealBebyggelseSomSkalRives;
         this.arealSumByggesak = props?.arealSumByggesak;
         this.beregnetGradAvUtnytting = props?.beregnetGradAvUtnytting;
+        this.beregnetMaksByggeareal = props?.beregnetMaksByggeareal;
+        this.parkeringsarealTerreng = props?.parkeringsarealTerreng;
+        this.tomtearealBeregnet = props?.tomtearealBeregnet;
+        this.tomtearealByggeomraade = props?.tomtearealByggeomraade;
+        this.tomtearealSomLeggesTil = props?.tomtearealSomLeggesTil;
+        this.tomtearealSomTrekkesFra = props?.tomtearealSomTrekkesFra;
     }
 }
