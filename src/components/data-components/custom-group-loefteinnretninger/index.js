4c5b3836490f31f53d1e69678d757cac33e561ed
Benjamin Dehli <superelg@gmail.com>
2025-12-08 13:54:05 +0100

    Adds Loefteinnretninger group component
    This commit introduces a new component for grouping Loefteinnretninger data.
    
    It provides a structured way to display information about lifting devices,
    including whether a lifting device is in a building, if one is planned,
    and a list of planned devices.

---
+++ src/components/data-components/custom-group-loefteinnretninger/index.js
@@ -0,0 +1,42 @@
+// Global functions
+import { instantiateComponent } from "../../../functions/componentHelpers.js";
+import { renderFeedbackListElement } from "../../../functions/feedbackHelpers.js";
+import { getComponentContainerElement, hasValue } from "../../../functions/helpers.js";
+
+// Local functions
+import {
+    renderEmptyFieldText,
+    renderErLoefteinnretningIBygningElement,
+    renderHeaderElement,
+    renderPlanlagteLoefteinnretningerElement,
+    renderPlanleggesLoefteinnretningIBygningElement
+} from "./renderers.js";
+
+export default customElements.define(
+    "custom-group-loefteinnretninger",
+    class extends HTMLElement {
+        async connectedCallback() {
+            const component = instantiateComponent(this);
+            const componentContainerElement = getComponentContainerElement(this);
+            if (component.hideIfEmpty && component.isEmpty && !!componentContainerElement) {
+                componentContainerElement.style.display = "none";
+            } else {
+                if (hasValue(component?.resourceValues?.title) && component?.hideTitle !== true) {
+                    this.appendChild(renderHeaderElement(component?.resourceValues?.title, component?.size));
+                }
+                if (component?.isEmpty) {
+                    const emptyFieldTextElement = renderEmptyFieldText(component);
+                    this.appendChild(emptyFieldTextElement);
+                } else {
+                    this.appendChild(renderErLoefteinnretningIBygningElement(component));
+                    this.appendChild(renderPlanleggesLoefteinnretningIBygningElement(component));
+                    this.appendChild(renderPlanlagteLoefteinnretningerElement(component));
+                    const feedbackListElement = component.hasValidationMessages && renderFeedbackListElement(component?.validationMessages);
+                    if (feedbackListElement) {
+                        this.appendChild(feedbackListElement);
+                    }
+                }
+            }
+        }
+    }
+);
