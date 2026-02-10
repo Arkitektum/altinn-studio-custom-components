ed93de833e004bc604bb7d85a5ce78c66b58a121
Benjamin Dehli <superelg@gmail.com>
2026-02-10 15:51:16 +0100

    Adds dataType to altinnStudioApps and subforms
    Adds the `dataType` property to each entry in the `altinnStudioApps` and `subforms` data arrays.
    This change allows for better identification and handling of data structures within the applications,
    which supports admin tools for package versions and layout previews.

--- api/data/altinnStudioApps.mjs
+++ api/data/altinnStudioApps.mjs
@@ -1,79 +1,98 @@
 const altinnStudioApps = [
     {
         appOwner: "dat",
-        appName: "byggesak-samtykke-v3"
+        appName: "byggesak-samtykke-v3",
+        dataType: "ArbeidstilsynetSamtykkeSoknad"
     },
     {
         appOwner: "dibk",
-        appName: "an-v2"
+        appName: "an-v2",
+        dataType: "AN"
     },
     {
         appOwner: "dibk",
-        appName: "disp-v1"
+        appName: "disp-v1",
+        dataType: "DS"
     },
     {
         appOwner: "dibk",
-        appName: "es-v2"
+        appName: "es-v2",
+        dataType: "ES"
     },
     {
         appOwner: "dibk",
-        appName: "et-v4"
+        appName: "et-v4",
+        dataType: "ET"
     },
     {
         appOwner: "dibk",
-        appName: "fa-v3"
+        appName: "fa-v3",
+        dataType: "FA"
     },
     {
         appOwner: "dibk",
-        appName: "fts-v1"
+        appName: "fts-v1",
+        dataType: "FTS"
     },
     {
         appOwner: "dibk",
-        appName: "ig-v3"
+        appName: "ig-v3",
+        dataType: "IG"
     },
     {
         appOwner: "dibk",
-        appName: "innsending-planforslag"
+        appName: "innsending-planforslag",
+        dataType: "OversendelseReguleringsplanforslag"
     },
     {
         appOwner: "dibk",
-        appName: "ko-v2"
+        appName: "ko-v2",
+        dataType: "KO"
     },
     {
         appOwner: "dibk",
-        appName: "mb-v3"
+        appName: "mb-v3",
+        dataType: "MB"
     },
     {
         appOwner: "dibk",
-        appName: "nabovarsel-svar-v5"
+        appName: "nabovarsel-svar-v5",
+        dataType: "NVS"
     },
     {
         appOwner: "dibk",
-        appName: "nabovarsel-v5"
+        appName: "nabovarsel-v5",
+        dataType: "NV"
     },
     {
         appOwner: "dibk",
-        appName: "rs-v4"
+        appName: "rs-v4",
+        dataType: "RS"
     },
     {
         appOwner: "dibk",
-        appName: "sa-v2"
+        appName: "sa-v2",
+        dataType: "SA"
     },
     {
         appOwner: "dibk",
-        appName: "su-v2"
+        appName: "su-v2",
+        dataType: "SU"
     },
     {
         appOwner: "dibk",
-        appName: "ta-v4"
+        appName: "ta-v4",
+        dataType: "TA"
     },
     {
         appOwner: "dibk",
-        appName: "ts-v1"
+        appName: "ts-v1",
+        dataType: "TS"
     },
     {
         appOwner: "dibk",
-        appName: "uttalelse-varselplanoppstart"
+        appName: "uttalelse-varselplanoppstart",
+        dataType: "Planuttalelse"
     },
     {
         appOwner: "dibk",
