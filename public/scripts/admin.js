25b20f3d145c53da341b46de60f2baceaefbea1b
Benjamin Dehli <superelg@gmail.com>
2026-02-10 09:47:40 +0100

    Implements admin tools sidebar and data fetching
    Adds admin tools sidebar rendering and data fetching.
    
    Fetches and prepares data related to display layouts,
    package versions, default text resources, and app resources.
    Computes resource usage, including missing resources and their
    usages within display layouts and app resource values.
    This data is then used to render the admin sidebar.

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,34 +1,27 @@
 // Local functions
-import { fetchDisplayLayouts } from "./apiHelpers.js";
+import { renderAdminSidebar } from "./adminRenderers.js";
+import { fetchAppResources, fetchDisplayLayouts, fetchPackageVersions } from "./apiHelpers.js";
 import { fetchDefaultTextResources } from "./getters.js";
-import {
-    renderDefaultTextResourcesList,
-    renderRadioButtonsFilterForTextResourcesList,
-    renderSelectApplicationFilterForTextResourcesList,
-    renderTextInputFilterForTextResourcesList
-} from "./textResourceUsageRenderers.js";
-import { getMissingResourceBindings, getResourceBindingsWithUsageFromApplications, getUsageForResources } from "./validators.js";
+import { getMissingResourceBindings, getResourceBindingsWithUsageFromApplications, getUsageForMissingResources, getUsageForResources } from "./validators.js";
 
 globalThis.onload = async function () {
     const defaultTextResources = await fetchDefaultTextResources("nb");
     globalThis.defaultTextResources = defaultTextResources;
     const layouts = await fetchDisplayLayouts();
+    globalThis.displayLayouts = layouts;
+    const packageVersions = await fetchPackageVersions();
+    globalThis.packageVersions = packageVersions;
+    const appResourceValues = await fetchAppResources("nb");
+    globalThis.appResourceValues = appResourceValues;
     const resourceBindingsInApplications = getResourceBindingsWithUsageFromApplications(layouts, "custom");
-    const { missingResourceBindings } = getMissingResourceBindings(resourceBindingsInApplications, globalThis.defaultTextResources);
-    const missingTextResourceBindingsUsage = getUsageForResources(
+    const { missingResourceBindings } = getMissingResourceBindings(resourceBindingsInApplications, null, globalThis.defaultTextResources);
+    const {missingResourcesUsage, missingResourcesWithLocalValueUsage} = getUsageForMissingResources(
         layouts,
-        missingResourceBindings.map((res) => ({ id: res }))
-    ).map((resBinding) => ({
-        ...resBinding,
-        missingFromDefaultTextResources: true
-    }));
+        missingResourceBindings.map((res) => ({ id: res })),
+        appResourceValues
+    );
     const textResourceUsage = getUsageForResources(layouts, globalThis.defaultTextResources?.resources);
-
-    const allTextResourceUsage = [...textResourceUsage, ...missingTextResourceBindingsUsage];
-
-    const mainAdminElement = document.getElementById("admin-main");
-    mainAdminElement.appendChild(renderRadioButtonsFilterForTextResourcesList(mainAdminElement, allTextResourceUsage));
-    mainAdminElement.appendChild(renderSelectApplicationFilterForTextResourcesList(mainAdminElement, allTextResourceUsage, layouts));
-    mainAdminElement.appendChild(renderTextInputFilterForTextResourcesList(mainAdminElement, allTextResourceUsage));
-    mainAdminElement.appendChild(renderDefaultTextResourcesList(allTextResourceUsage, allTextResourceUsage));
+    const allTextResourceUsage = [...textResourceUsage, ...missingResourcesUsage, ...missingResourcesWithLocalValueUsage];
+    globalThis.allTextResourceUsage = allTextResourceUsage;
+    renderAdminSidebar();
 };
