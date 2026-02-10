87e021a09ce1f90f6123f339a8c57260cbcab54b
Benjamin Dehli <superelg@gmail.com>
2026-02-10 08:39:54 +0100

    Adds API endpoints for package versions and resources
    Exposes API endpoints to retrieve package versions and
    application resource values. This provides necessary data
    for admin tools related to package management and layout previews.

--- api/index.mjs
+++ api/index.mjs
@@ -4,7 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 
 // Local functions
-import { getDisplayLayouts } from "./scripts/functions.mjs";
+import { getAppResourceValues, getDisplayLayouts, getPackageVersions } from "./scripts/functions.mjs";
 
 const app = express();
 const port = process.env.API_PORT;
@@ -22,5 +22,25 @@ app.get("/api/displayLayouts", async (req, res) => {
     } catch (error) {
         console.error("Error fetching display layouts:", error);
         res.status(500).json({ error: "Failed to fetch display layouts" });
+    }
+});
+
+app.get("/api/packageVersions", async (req, res) => {
+    try {
+        const packageVersions = await getPackageVersions();
+        res.json(packageVersions);
+    } catch (error) {
+        console.error("Error fetching package.json files:", error);
+        res.status(500).json({ error: "Failed to fetch package.json files" });
+    }
+});
+
+app.get("/api/appResources", async (req, res) => {
+    try {
+        const appResources = await getAppResourceValues("nb");
+        res.json(appResources);
+    } catch (error) {
+        console.error("Error fetching app resource values:", error);
+        res.status(500).json({ error: "Failed to fetch app resource values" });
     }
 });
