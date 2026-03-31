8dc6d0de7021c995bb6c0e619db90fb67e7d390e
Benjamin Dehli <superelg@gmail.com>
2026-03-31 11:37:12 +0200

    Update Dispensasjon class to match updated sub-form schema
    Updates the Dispensasjon layout class to align with changes in the data structure. The class now includes additional properties for plan identification, bestemmelsestype, and dispensasjonstema, while refactoring tiltakstyper to map directly to Kode instances.

--- src/classes/layout-classes/Dispensasjon.js
+++ src/classes/layout-classes/Dispensasjon.js
@@ -1,15 +1,15 @@
 // Classes
 import Begrunnelse from "../data-classes/Begrunnelse.js";
-import DispensasjonBeskrivelse from "../data-classes/DispensasjonBeskrivelse.js";
-import DispensasjonFra from "../data-classes/DispensasjonFra.js";
 import EiendomByggested from "../data-classes/EiendomByggested.js";
 import Kode from "../data-classes/Kode.js";
 import KommunensSaksnummer from "../data-classes/KommunensSaksnummer.js";
 import Metadata from "../data-classes/Metadata.js";
 import Part from "../data-classes/Part.js";
 import Stedfesting from "../data-classes/Stedfesting.js";
-import Tiltakstyper from "../data-classes/Tiltakstyper.js";
 import Varighet from "../data-classes/Varighet.js";
+
+// Global functions
+import { hasValue } from "@arkitektum/altinn-studio-custom-components-utils";
 
 /**
  * Class representing a Dispensasjon.
@@ -17,38 +17,76 @@ import Varighet from "../data-classes/Varighet.js";
  */
 export default class Dispensasjon {
     /**
-     * Constructs a new instance of the Dispensasjon class.
-     *
-     * @param {Object} props - The properties to initialize the Dispensasjon instance.
-     * @param {DispensasjonBeskrivelse} [props.dispensasjonBeskrivelse] - The description of the dispensasjon.
-     * @param {string} [props.dispensasjonReferanse] - The reference for the dispensasjon.
-     * @param {Kode} [props.soeknadstype] - The type of application.
-     * @param {KommunensSaksnummer} [props.kommunensSaksnummer] - The municipality's case number.
-     * @param {Metadata} [props.metadata] - Metadata associated with the dispensasjon.
-     * @param {EiendomByggested} [props.eiendomByggested] - The property or construction site information.
-     * @param {Tiltakstyper} [props.tiltakstyper] - The types of measures involved.
-     * @param {Part} [props.tiltakshaver] - The party responsible for the tiltak (measure).
-     * @param {Part} [props.ansvarligSoeker] - The responsible applicant.
-     * @param {DispensasjonFra} [props.dispensasjonFra] - Information about what the dispensasjon is from.
-     * @param {Stedfesting} [props.stedfesting] - Geographical location information.
-     * @param {Varighet} [props.varighet] - The duration of the dispensasjon.
-     * @param {Begrunnelse} [props.begrunnelse] - The justification for the dispensasjon.
-     * @param {string} [props.generelleVilkaar] - General conditions for the dispensasjon.
+     * Creates an instance of Dispensasjon.
+     * @param {Object} props - The properties object.
+     * @param {Object} [props.tiltakstyper] - The tiltakstyper object containing type information.
+     * @param {Object} [props.begrunnelse] - The begrunnelse object containing justification information.
+     * @param {Object} [props.bestemmelsestype] - The bestemmelsestype object containing type information.
+     * @param {string} [props.dispensasjonsbeskrivelse] - The dispensasjonsbeskrivelse string containing the description of the exemption.
+     * @param {string} [props.dispensasjonsreferanse] - The dispensasjonsreferanse string containing the reference for the exemption.
+     * @param {Object} [props.dispensasjonstema] - The dispensasjonstema object containing theme information.
+     * @param {Object} [props.eiendomByggested] - The eiendomByggested object containing property and construction site information.
+     * @param {Array|string|boolean} [props.generelleVilkaar] - The generelleVilkaar array or string containing general conditions for the exemption.
+     * @param {Object} [props.kommunensSaksnummer] - The kommunensSaksnummer object containing the municipality's case number information.
+     * @param {Object} [props.metadata] - The metadata object containing metadata information.
+     * @param {Object} [props.nasjonalArealplanId] - The nasjonalArealplanId object containing national area plan identification information.
+     * @param {string} [props.paragrafnummer] - The paragrafnummer string containing the paragraph number related to the exemption.
+     * @param {string} [props.plannavn] - The plannavn string containing the name of the plan related to the exemption.
+     * @param {Object} [props.stedfesting] - The stedfesting object containing geolocation information.
+     * @param {Object} [props.tiltakshaver] - The tiltakshaver object containing information about the party responsible for the measure.
+     * @param {Object} [props.varighet] - The varighet object containing duration information for the exemption.
      */
     constructor(props) {
-        this.dispensasjonBeskrivelse = props?.dispensasjonBeskrivelse && new DispensasjonBeskrivelse(props.dispensasjonBeskrivelse);
-        this.dispensasjonReferanse = props?.dispensasjonReferanse;
-        this.soeknadstype = props?.soeknadstype && new Kode(props.soeknadstype);
+        const tiltakstyper = props ? this.getTiltakstyperFromProps(props) : null;
+        this.begrunnelse = props?.begrunnelse && new Begrunnelse(props.begrunnelse);
+        this.bestemmelsestype = props?.bestemmelsestype && new Kode(props.bestemmelsestype);
+        this.dispensasjonsbeskrivelse = props?.dispensasjonsbeskrivelse;
+        this.dispensasjonsreferanse = props?.dispensasjonsreferanse;
+        this.dispensasjonstema = props?.dispensasjonstema && new Kode(props.dispensasjonstema);
+        this.eiendomByggested = props?.eiendomByggested && new EiendomByggested(props.eiendomByggested);
+        this.generelleVilkaar = props?.generelleVilkaar;
         this.kommunensSaksnummer = props?.kommunensSaksnummer && new KommunensSaksnummer(props.kommunensSaksnummer);
         this.metadata = props?.metadata && new Metadata(props.metadata);
-        this.eiendomByggested = props?.eiendomByggested && new EiendomByggested(props.eiendomByggested);
-        this.tiltakstyper = props?.tiltakstyper && new Tiltakstyper(props.tiltakstyper);
-        this.tiltakshaver = props?.tiltakshaver && new Part(props.tiltakshaver);
-        this.ansvarligSoeker = props?.ansvarligSoeker && new Part(props.ansvarligSoeker);
-        this.dispensasjonFra = props?.dispensasjonFra && new DispensasjonFra(props.dispensasjonFra);
+        this.nasjonalArealplanId = props?.nasjonalArealplanId && new Kode(props.nasjonalArealplanId);
+        this.paragrafnummer = props?.paragrafnummer;
+        this.plannavn = props?.plannavn;
         this.stedfesting = props?.stedfesting && new Stedfesting(props.stedfesting);
+        this.tiltakshaver = props?.tiltakshaver && new Part(props.tiltakshaver);
         this.varighet = props?.varighet && new Varighet(props.varighet);
-        this.begrunnelse = props?.begrunnelse && new Begrunnelse(props.begrunnelse);
-        this.generelleVilkaar = props?.generelleVilkaar;
+
+        if (tiltakstyper) {
+            this.tiltakstyper = tiltakstyper;
+        }
+    }
+
+    /**
+     * Extracts the type information from the provided properties.
+     * @param {Object} props - The properties object.
+     * @param {Object} [props.tiltakstyper] - The tiltakstyper object containing type information.
+     * @param {Array} [props.tiltakstyper.kode] - An array of type codes.
+     * @returns {Object|null} An object containing the extracted type information, or null if not available.
+     */
+    getTiltakstyperFromProps(props) {
+        if (props && hasValue(props?.tiltakstyper)) {
+            return {
+                kode: this.getKodeFromType(props.tiltakstyper)
+            };
+        }
+        return null;
+    }
+
+    /**
+     * Converts the provided type object into an array of Kode instances.
+     * @param {Object} type - The type object containing type information.
+     * @param {Array} [type.kode] - An array of type codes.
+     * @returns {Array|null} An array of Kode instances, or null if no valid codes are found.
+     */
+    getKodeFromType(type) {
+        if (Array.isArray(type?.kode) && type?.kode?.length) {
+            return type?.kode?.map((item) => {
+                return new Kode(item);
+            });
+        }
+        return null;
     }
 }
