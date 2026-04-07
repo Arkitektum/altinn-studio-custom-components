5d30766f70e0c545032e916102b6a40b68aa24e1
Mantas Lapinskas <mantas@arkitektum.no>
2026-04-07 15:51:29 +0200

    Rename erTillatelseGittRiksFylkesVeg to erTillatelseGittRiksFylkesveg
    Updates the property name and its JSDoc in the Adkomst class to use a lowercase 'v' in "veg", aligning with standardized naming conventions.

--- src/classes/data-classes/Adkomst.js
+++ src/classes/data-classes/Adkomst.js
@@ -10,7 +10,7 @@ import Vegtype from "./Vegtype.js";
  * @param {Array<Object>} [props.vegtype] - Array of vegtype items to be mapped to Vegtype instances.
  * @param {boolean} [props.erTillatelseGittKommunalVeg] - Indicates if permission is granted for municipal roads.
  * @param {boolean} [props.erTillatelseGittPrivatVeg] - Indicates if permission is granted for private roads.
- * @param {boolean} [props.erTillatelseGittRiksFylkesVeg] - Indicates if permission is granted for national or county roads.
+ * @param {boolean} [props.erTillatelseGittRiksFylkesveg] - Indicates if permission is granted for national or county roads.
  */
 export default class Adkomst {
     constructor(props) {
@@ -18,6 +18,6 @@ export default class Adkomst {
         this.vegtype = props?.vegtype && new Vegtype(props.vegtype);
         this.erTillatelseGittKommunalVeg = props?.erTillatelseGittKommunalVeg;
         this.erTillatelseGittPrivatVeg = props?.erTillatelseGittPrivatVeg;
-        this.erTillatelseGittRiksFylkesVeg = props?.erTillatelseGittRiksFylkesVeg;
+        this.erTillatelseGittRiksFylkesveg = props?.erTillatelseGittRiksFylkesveg;
     }
 }
