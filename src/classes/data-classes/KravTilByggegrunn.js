5b13cc6c43f228840e29fd30d8e7ed1070b05a46
Benjamin Dehli <superelg@gmail.com>
2026-04-17 15:07:09 +0200

    Rename miljoeforhold to harMiljoeforhold in KravTilByggegrunn
    Updates the property to a boolean indicator to represent whether environmental conditions are present, rather than a descriptive string.

--- src/classes/data-classes/KravTilByggegrunn.js
+++ src/classes/data-classes/KravTilByggegrunn.js
@@ -7,11 +7,11 @@ import MuligeOmraadeRisikoer from "./MuligeOmraadeRisikoer.js";
  * @class KravTilByggegrunn
  * @param {Object} props - The properties to initialize the KravTilByggegrunn instance.
  * @param {Object} [props.muligeOmraadeRisikoer] - The data for possible area risks, used to create a MuligeOmraadeRisikoer instance.
- * @param {string} [props.miljoeforhold] - Environmental conditions related to the building ground.
+ * @param {boolean} [props.harMiljoeforhold] - Indicates if there are environmental conditions related to the building ground.
  */
 export default class KravTilByggegrunn {
     constructor(props) {
         this.muligeOmraadeRisikoer = props?.muligeOmraadeRisikoer && new MuligeOmraadeRisikoer(props.muligeOmraadeRisikoer);
-        this.miljoeforhold = props?.miljoeforhold;
+        this.harMiljoeforhold = props?.harMiljoeforhold;
     }
 }
