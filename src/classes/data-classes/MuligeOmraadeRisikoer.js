e6e9275509e56b4395b3dbf00ef33c0f3b4ed54f
Benjamin Dehli <superelg@gmail.com>
2026-04-17 15:06:57 +0200

    Update MuligeOmraadeRisikoer to handle multiple area risks
    Changes the data class to expect an array of risk objects instead of a single object, mapping each entry to an Omraaderisiko instance.

--- src/classes/data-classes/MuligeOmraadeRisikoer.js
+++ src/classes/data-classes/MuligeOmraadeRisikoer.js
@@ -6,10 +6,11 @@ import Omraaderisiko from "./Omraaderisiko.js";
  *
  * @class MuligeOmraadeRisikoer
  * @param {Object} props - The properties to initialize the MuligeOmraadeRisikoer instance.
- * @param {Object} [props.omraadeRisiko] - The area risk data used to create an Omraaderisiko instance.
+ * @param {Array<Object>} [props.omraadeRisiko] - An array of area risk objects to be transformed into Omraaderisiko instances.
  */
 export default class MuligeOmraadeRisikoer {
     constructor(props) {
-        this.omraadeRisiko = props?.omraadeRisiko && new Omraaderisiko(props.omraadeRisiko);
+        this.omraadeRisiko =
+            props?.omraadeRisiko?.length && Array.isArray(props.omraadeRisiko) ? props.omraadeRisiko.map((item) => new Omraaderisiko(item)) : null;
     }
 }
