9921aeaf00447e849df94a2df009d6e04f760026
Benjamin Dehli <superelg@gmail.com>
2026-05-07 13:33:29 +0200

    Add CustomGroupDispensasjonOversikt component
    This new custom web component displays an overview of dispensation data. It orchestrates the rendering of titles, counts, tables, and validation feedback, incorporating conditional visibility for empty states and developer tools integration.

---
+++ src/components/data-components/custom-group-dispensasjon-oversikt/index.js
@@ -0,0 +1,45 @@
+// Dependencies
+import { hasValue } from "@arkitektum/altinn-studio-custom-components-utils";
+
+// Global functions
+import { addDevToolsOverlay, isDevMode, renderHiddenDevToolsElement } from "../../../functions/devToolsHelpers.js";
+import { getComponentContainerElement } from "../../../functions/helpers.js";
+import { instantiateComponent } from "../../../functions/componentHelpers.js";
+import { renderFeedbackListElement } from "../../../functions/feedbackHelpers.js";
+
+// Local functions
+import { renderDispensasjonCount, renderDispensasjonTable, renderEmptyFieldText, renderHeaderElement } from "./renderers.js";
+
+export default customElements.define(
+    "custom-group-dispensasjon-oversikt",
+    class extends HTMLElement {
+        async connectedCallback() {
+            const component = instantiateComponent(this);
+            const componentContainerElement = getComponentContainerElement(this);
+            if (component.hideIfEmpty && component.isEmpty && !!componentContainerElement) {
+                if (isDevMode()) {
+                    const hiddenEl = renderHiddenDevToolsElement(this, component, "data");
+                    if (hiddenEl) this.appendChild(hiddenEl);
+                } else {
+                    componentContainerElement.style.display = "none";
+                }
+            } else {
+                if (hasValue(component?.resourceValues?.title) && component?.hideTitle !== true) {
+                    this.appendChild(renderHeaderElement(component?.resourceValues?.title, component?.size));
+                }
+                if (component?.isEmpty) {
+                    const emptyFieldTextElement = renderEmptyFieldText(component);
+                    this.appendChild(emptyFieldTextElement);
+                } else {
+                    this.appendChild(renderDispensasjonCount(component));
+                    this.appendChild(renderDispensasjonTable(component));
+                }
+                addDevToolsOverlay(this, component, "data");
+            }
+            const feedbackListElement = component?.hasValidationMessages && renderFeedbackListElement(component?.validationMessages);
+            if (feedbackListElement) {
+                this.appendChild(feedbackListElement);
+            }
+        }
+    }
+);
