4a1815ff25756574122b98cee80d30846f4684ad
Benjamin Dehli <benjamin@arkitektum.no>
2026-07-08 13:22:42 +0200

    Add class for CustomMatrix

---
+++ src/classes/system-classes/component-classes/CustomMatrix.js
@@ -0,0 +1,39 @@
+// Dependencies
+import { hasValue } from "@arkitektum/altinn-studio-custom-components-utils";
+
+// Classes
+import CustomComponent from "../CustomComponent.js";
+
+/**
+ * CustomMatrix is a custom component class that represents a matrix with resource-based content.
+ * It extends the CustomComponent class and provides functionality to determine if the matrix has content.
+ *
+ * @class
+ * @extends CustomComponent
+ *
+ * @param {Object} props - The properties object for the component.
+ * @param {Object} [props.resourceValues] - An object containing resource values for the matrix.
+ * @param {*} [props.resourceValues.data] - The data value to check for content in the matrix.
+ *
+ * @property {boolean} isEmpty - Indicates whether the matrix has content.
+ * @property {Object} resourceValues - Stores the resource values provided in props.
+ */
+export default class CustomMatrix extends CustomComponent {
+    constructor(props) {
+        super(props);
+        this.isEmpty = !this.hasContent(props);
+        this.resourceValues = props?.resourceValues || {};
+    }
+
+    /**
+     * Determines if the provided props contain content in the `resourceValues.data` property.
+     *
+     * @param {Object} props - The properties object to check.
+     * @param {Object} [props.resourceValues] - An object containing resource values.
+     * @param {*} [props.resourceValues.data] - The text value to check for content.
+     * @returns {boolean} Returns `true` if `resourceValues.data` has a value, otherwise `false`.
+     */
+    hasContent(props) {
+        return hasValue(props?.resourceValues?.data);
+    }
+}
--- src/functions/componentHelpers.js
+++ src/functions/componentHelpers.js
@@ -62,6 +62,7 @@ import CustomList from "../classes/system-classes/component-classes/CustomList.j
 import CustomListData from "../classes/system-classes/component-classes/CustomListData.js";
 import CustomListPlanlagteLoefteinnretninger from "../classes/system-classes/component-classes/CustomListPlanlagteLoefteinnretninger.js";
 import CustomListVedlegg from "../classes/system-classes/component-classes/CustomListVedlegg.js";
+import CustomMatrix from "../classes/system-classes/component-classes/CustomMatrix.js";
 import CustomParagraph from "../classes/system-classes/component-classes/CustomParagraph.js";
 import CustomParagraphText from "../classes/system-classes/component-classes/CustomParagraphText.js";
 import CustomSubheaderText from "../classes/system-classes/component-classes/CustomSubheaderText.js";
@@ -149,6 +150,7 @@ export function getComponentForTagName(tagName) {
         "custom-list-data": CustomListData,
         "custom-list-planlagte-loefteinnretninger": CustomListPlanlagteLoefteinnretninger,
         "custom-list-vedlegg": CustomListVedlegg,
+        "custom-matrix": CustomMatrix,
         "custom-paragraph": CustomParagraph,
         "custom-paragraph-text": CustomParagraphText,
         "custom-subheader-text": CustomSubheaderText,
